371903373994095515fa2ec9a34991722efe8973
emnakotti1 <kottiemna.pro@gmail.com>
2026-01-27 11:47:47 +0100

    first1

---
+++ src/main/java/tn/esprit/kottiemna4arctic3/KottiEmna4arctic3Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.kottiemna4arctic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KottiEmna4arctic3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KottiEmna4arctic3Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/kottiemna4arctic3/KottiEmna4arctic3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.kottiemna4arctic3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KottiEmna4arctic3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
